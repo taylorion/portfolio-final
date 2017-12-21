30a4440715d0991557422457504e0c94d066ddd5
taylorion <taylorion95@gmail.com>
2017-12-21 17:19:32 -0500

    Added parallaxing to all header images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-var parallaxitems=document.querySelectorAll(".parallax-img");
-var parallaxtops = [];
+var parallaxitems = document.querySelectorAll('.parallax-img');
 
-[].forEach.call(parallaxitems, function(item){
-  item.style.backgroundAttachment="local";
-  parallaxtops.push(item.getBoundingClientRect().top);
-});
+window.addEventListener('scroll', function(){
+  [].forEach.call(parallaxitems, function (item, i) {
+    var diffParentTop = Math.floor(item.parentNode.getBoundingClientRect().top);
+    var diffParentBottom = Math.floor(item.parentNode.getBoundingClientRect().bottom);
+    var isOnScreen = ((diffParentTop <= window.innerHeight) && (diffParentBottom > 0));
+    var diff;
 
-window.addEventListener("scroll", function(){
-  [].forEach.call(parallaxitems, function(item, i){
-    var diff= Math.round(((window.scrollY > parallaxtops[i]) ? window.scrollY - parallaxtops[i] : window.scrollY) / 5); /*The number at the end controlls scroll speed*/
-    var bgTop=Math.round(parseInt(getComputedStyle(item).blockSize,10)/2);
-console.log(parallaxtops[i], window.scrollY);
-    // item.style.backgroundPosition="50% calc((50% - "+diff+"px - "+bgTop+"px) * -1)";
-    item.style.marginTop="-"+diff+"px";
+    if (isOnScreen) {
+      diff = Math.round(diffParentBottom / 5);
+      item.style.marginTop = '-' + diff + 'px';
+    }
   });
 });
